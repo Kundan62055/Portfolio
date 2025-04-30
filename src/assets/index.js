030ab9d484305caa3fec3e241f695868e19006a7
Kundan62055 <kk6205585@gmail.com>
2025-04-30 20:23:24 +0530

    first commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,12 +20,20 @@ import CompileVortex from "./projects/CompileVortex.png";
 // Import tech logos
 import css from "./tech/css.png";
 import git from "./tech/git.png";
+import vscode from "./tech/vscode.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import tailwind from "./tech/tailwind.png";
-import threejs from "./tech/threejs.png";
+// import bootstrap from "./tech/bootstarp.png";
+import mongodb from "./tech/mongodb.png";
+import reactnative from "./tech/reactnative.png";
+// import angularjs from "./tech/angularjs.png";
+import nextjs from "./tech/nextjs.png";
+import expressjs from "./tech/expressjs.png";
+// import sql from "./tech/sql.png";
+// import threejs from "./tech/threejs.png";
 
 // Import common assets
 import logo from "./logo.svg";
@@ -47,8 +55,16 @@ export {
   javascript,
   nodejs,
   reactjs,
+  reactnative,
+  nextjs,
+  expressjs,
+  vscode,
+  mongodb,
+  // angularjs,
+  // sql,
   tailwind,
-  threejs,
+  // bootstrap,
+  // threejs,
   weatherpedia,
   termpw,
   payloadmaster,
